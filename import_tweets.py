f649769f331ea38e76a8a858c0691328c176622b
Oleg Boiko <oleh.boiko@gmail.com>
2016-12-05 23:26:49 -0800

    Initial commit

---
+++ import_tweets.py
@@ -0,0 +1,15 @@
+import tweepy
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.user_timeline()
+for tweet in public_tweets:
+    print tweet.text
