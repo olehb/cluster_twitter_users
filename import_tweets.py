efbcbcfb0245e8036670e2fe32c6a3093b0a8674
Oleg Boiko <oleh.boiko@gmail.com>
2016-12-05 23:45:01 -0800

    Basic Tweets dumping

--- import_tweets.py
+++ import_tweets.py
@@ -1,15 +1,24 @@
 import tweepy
+import json
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+MAX_RECORDS = 1
+
+consumer_key = 'wxwczZwiA3peVZZWq9qp3w'
+consumer_secret = 'IfD3AvPwW2DsetwTupcmQPqU0rseqtM9YNPd3e2FNgM'
+access_token = '22168865-9rIa3yIpBceZotEC4EIODh2a82fTiCqn3yow1ZoQs'
+access_token_secret = 'zogWTsVjWlkwqD0hIIf63Pu5rlX9elfAC0mOGOpbeZj4o'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.user_timeline()
-for tweet in public_tweets:
-    print tweet.text
+for i, user_id in enumerate(open('user_ids.csv', 'r')):
+    user_id = user_id.strip()
+    tweets = api.user_timeline(user_id=user_id)
+
+    with open('data/%s.json' % user_id, 'w') as f:
+        json.dump([tweet._json for tweet in tweets], f)
+
+    if MAX_RECORDS > 0 and i > MAX_RECORDS-2:
+        break
